5b83cf1ce13f31ed4ae452b381db33d7ebcd8591
mironiukia <ilia.mironyuk21@gmail.com>
2023-11-10 19:57:32 +0400

    fix

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -4,7 +4,7 @@ import ThemeIconDeprecated from '@/shared/assets/icons/theme-light.svg';
 import ThemeIcon from '@/shared/assets/icons/theme.svg';
 import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
-import { saveJsonSettings } from '@/entities/User';
+// import { saveJsonSettings } from '@/entities/User';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Icon as IconDeprecated } from '@/shared/ui/deprecated/Icon';
 import { ToggleFeatures } from '@/shared/lib/features';
@@ -19,10 +19,8 @@ export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
     const dispatch = useAppDispatch();
 
     const onToggleHandler = useCallback(() => {
-        toggleTheme((newTheme) => {
-            dispatch(saveJsonSettings({ theme: newTheme }));
-        });
-    }, [dispatch, toggleTheme]);
+
+    }, []);
 
     return (
         <ToggleFeatures
