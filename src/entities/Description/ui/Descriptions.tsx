7c98d01f36edc5b86a2f16e92dd82c907966fe0e
mironiukia <ilia.mironyuk21@gmail.com>
2023-11-17 11:02:13 +0400

    туда же фикс

--- src/entities/Description/ui/Descriptions.tsx
+++ src/entities/Description/ui/Descriptions.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import {useTranslation} from "react-i18next";
 import { Text } from '@/shared/ui/redesigned/Text';
 import { Button } from '@/shared/ui/redesigned/Button';
 import { Card } from '@/shared/ui/redesigned/Card';
@@ -6,31 +7,31 @@ import vk from '@/shared/assets/icons/vk.jpg';
 import rutube from '@/shared/assets/icons/rutube.jpg';
 import telegramm from '@/shared/assets/icons/телеграф.png';
 import cls from './Description.module.scss';
-import text from './shared/const/text'
+import {text} from '@/shared/const/text'
 
 export const Description = () => {
 
     const navigate = useNavigate();
 
+    const {t} = useTranslation()
+
     const handleRedirect = () => {
         navigate('/', { replace: true });
     };
 
-    const handleLinks = (src) => {
+    const handleLinks = (src: string): void => {
         window.open(src);
     };
 
     return (
         <>
             <Text />
-            <Card
-                children={
+            <Card>
                 <Text text={text} />
-                }
-            />
+            </Card>
             <br />
             <Button size="m" onClick={handleRedirect}>
-                На главную
+                {t('На главную')}
             </Button>
             <hr className={cls.hrContent} />
 
